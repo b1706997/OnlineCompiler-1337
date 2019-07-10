a762c602a64026676908734bf0d401a05da6da0b
Nguyen Nhat Son <nguyennhatson1810@gmail.com>
2019-07-10 10:35:10 +0700

    TrustMeCommit2

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const app = express();
+const pug = {
+  test: /\.pug$/,
+  use: ['html-loader?attrs=false', 'pug-html-loader']
+};
 app.use(express.static(__dirname + '/public'));  // CSS use 
-const server = app.listen(process.env.PORT || 1111,()=> {
+const server = app.listen(process.env.PORT || 1111,err => {
+	if(err) throw err;
+	
     console.log(`Express running -> PORT ${server.address().port}`);
-})
+});
 app.set('view engine','pug');
 app.get('/',(req,res) => {
     res.render('HTML');
-})
+});
