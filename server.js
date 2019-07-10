10ad5651757ae9be69d2964bc66f25c75e71bd77
Nguyen Nhat Son <nguyennhatson1810@gmail.com>
2019-07-10 10:46:50 +0700

    jkasjdk

--- server.js
+++ server.js
@@ -1,10 +1,6 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const pug = {
-  test: /\.pug$/,
-  use: ['html-loader?attrs=false', 'pug-html-loader']
-};
 app.use(express.static(__dirname + '/public'));  // CSS use 
 const server = app.listen(process.env.PORT || 1111,err => {
 	if(err) throw err;
@@ -14,5 +10,5 @@ const server = app.listen(process.env.PORT || 1111,err => {
 app.set('view engine','pug');
 app.set("views", path.join(__dirname, "views"));
 app.get('/',(req,res) => {
-    res.render('HTML');
+    res.render('index');
 });
