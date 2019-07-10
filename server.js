fa5d21c534f60dc5e5f8b6fd93a79fbf7702f46e
Nguyen Nhat Son <nguyennhatson1810@gmail.com>
2019-07-10 09:36:12 +0700

    ThirdCommit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 app.use(express.static(__dirname + '/public'));  // CSS use 
-const server = app.listen(1111,()=> {
+const server = app.listen(process.env.PORT || 1111,()=> {
     console.log(`Express running -> PORT ${server.address().port}`);
 })
 app.set('view engine','pug');
