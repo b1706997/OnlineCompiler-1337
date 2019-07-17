98a32a1f56f5c0667fa3185bb30097fe04f93ed4
Nguyen Nhat Son <nguyennhatson1810@gmail.com>
2019-07-17 09:06:45 +0700

    hgh

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
 app.use(express.static(__dirname + '/public'));  // CSS use 
@@ -7,8 +8,18 @@ const server = app.listen(process.env.PORT || 1111,err => {
 	
     console.log(`Express running -> PORT ${server.address().port}`);
 });
+app.use(bodyParser.urlencoded({  // parse req body 
+    extended: true
+  }));
 app.set('view engine','pug');
 app.set("views", path.join(__dirname, "views"));
 app.get('/',(req,res) => {
     res.render('index');
+});
+app.post('/getLang', (req,res) => {
+    const lang = req.body.languagePicker;
+    if(lang==="HTML/CSS")
+        res.render('HTML_CSS');
+    else
+        res.render(lang);
 });
