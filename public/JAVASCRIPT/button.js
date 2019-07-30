b03591852f35918dc498bfca725508dadd3812e6
Nguyen Nhat Son <nguyennhatson1810@gmail.com>
2019-07-30 11:46:41 +0700

    LastCommit

--- public/JAVASCRIPT/button.js
+++
@@ -1,8 +0,0 @@
-console.log('Client-side code running');
-
-const button = document.getElementById('runbutton');
-button.addEventListener('click', function(e) {
-  var editor = document.getElementById('editor');
-  const a = editor.getValue();
-  console.log(a);
-});
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const decodeUriComponent = require('decode-uri-component');
 const path = require('path');
 const fs = require('fs');
 const app = express();
@@ -23,14 +24,71 @@ app.post('/getLang', (req,res) => {
         res.render('HTML_CSS');
     else
         res.render(lang);
-    app.post('/run',(req,res) => {
+    app.use(bodyParser.text());
+    app.post('/getLang/run',(req,res) => {
         console.log('Server route running');
         const code = req.body; // code => still an object not yet a string
         const codePath = path.join(__dirname,"public","CODE","code.txt"); //   PROJECT/public/CODE/code.txt is created
         const stream = fs.createWriteStream(codePath); // create a write stream
-        stream.write(JSON.stringify(code)); // save the stringify object
+        stream.write(code); // save the stringify object
         stream.end();
         console.log(code);
-        res.end();
-    });
+
+        // change the code file name to the correct language 
+        if(lang==='C++')
+        {
+            var newPath = path.join(__dirname,"public","CODE","code.cpp");
+        }
+        else if(lang==='Javascript')
+        {
+            var newPath = path.join(__dirname,"public","CODE","code.js");
+        }
+        else if(lang==='Java')
+        {
+            var newPath = path.join(__dirname,"public","CODE","code.java");
+        }
+        else if(lang==='Python')
+        {
+            var newPath = path.join(__dirname,"public","CODE","code.pyd");
+        }
+        //Rename
+        fs.rename(codePath,newPath,function(err) {
+            if (err) throw err;
+
+            console.log('File name changed');
+        });
+
+        // compile a file 
+        var spawn = require('child_process').spawn;
+        // language switch
+        if(lang==='C++')
+        {
+            var compile = spawn('gcc', ['public/CODE/code.cpp'],['-lstdc++']);
+        }
+        else if(lang==='Java')
+        {
+            var compile = spawn('javac',['public/CODE/code.java']);
+        }
+        
+        compile.stdout.on('data', function (data) {
+            console.log('stdout: ' + data);
+        });
+        compile.stderr.on('data', function (data) {
+            console.log(String(data));
+            res.send(data);
+        });
+        compile.on('close', function (data) {
+            if (data === 0) {
+                var run = spawn('./a.exe', []);
+                run.stdout.on('data', function (output) {
+                    console.log(String(output));
+                    res.send(output);
+                });
+                run.stderr.on('data', function (output) {
+                    console.log(String(output));
+                    res.send(output);
+                });
+            } 
+        }) 
+    }) 
 });
